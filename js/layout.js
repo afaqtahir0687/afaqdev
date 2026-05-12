f84c7e6d399ecd9bc11470ae2434da8030c9ad56
Murtaza395 <mughalmurtaza999@gmail.com>
2026-05-12 15:42:38 +0500

    payment integrate

--- js/layout.js
+++ js/layout.js
@@ -23,6 +23,7 @@ const layouts = {
                         <li class="nav-item"><a class="nav-link" href="#experience">Experience</a></li>
                         <li class="nav-item"><a class="nav-link" href="#education">Education</a></li>
                         <li class="nav-item"><a class="nav-link" href="#projects">Projects</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#integrations">Integrations</a></li>
                         <li class="nav-item"><a class="nav-link" href="#contact">Contact</a></li>
                     </ul>
                 </div>
